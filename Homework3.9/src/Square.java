c123c30da512eb9447d5c76a8912a86f82ac95b4
Liliana Adelina Călin <lilianaadelina240@gmail.com>
2023-05-20 14:30:59 +0300

    Define a Shape interface. Extend it to Circle, Square and Rectangle. Use SOLID principles to compute the area of couple of different Shape instances.

---
+++ Homework3.9/src/Square.java
@@ -0,0 +1,14 @@
+class Square implements Shape
+{
+    private double sideLength;
+
+    public Square(double sideLength)
+    {
+        this.sideLength = sideLength;
+    }
+    @Override
+    public double calculateArea()
+    {
+        return sideLength * sideLength;
+    }
+}
